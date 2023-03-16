49b18499d605f3c4e90fc091ece69eb9a526bf30
Danielemiccoli-dev <102368221+Danielemiccoli-dev@users.noreply.github.com>
2023-03-16 10:50:50 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// DOM elements
 const input = document.getElementById("main-input");
 const mainWrapper = document.getElementById("main-wrapper")
 const searchButton = document.getElementById("search-button");
+const successArea = document.getElementById("success-area")
 const mainKeyWrapper = document.querySelector(".main-key-wrapper")
 const shadow = document.querySelector(".main-key-wrapper_shadow")
+
+// utils function
+function copyToNote(text) {
+  const tempInput = document.createElement("input");
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
+function truncateAddress (address) {
+  return `${address.substr(0, 5)}...${address.substr(
+      address.length - 5,
+      address.length
+  )}`;
+}
 
 /* Dan */
 //------------------
@@ -10,13 +28,14 @@ const shadow = document.querySelector(".main-key-wrapper_shadow")
 fetch("whitelist.json")
   .then(response => response.json())
   .then(usersData => {
+    // whitelist data
     const whitelist = usersData
     const whiteListLeaves = whitelist.map(addr => keccak256(addr))
     const tree = new window.MerkleTree(whiteListLeaves, keccak256, {sortPairs: true})
     const rootHash = tree.getRoot()
-    console.log(rootHash.toString("hex"));
     const claimingAddress = whiteListLeaves[5]; //Minting address
-    console.log(claimingAddress)
+
+    // event listener for search and input
     searchButton.addEventListener("click", () => {
       searchKeyFromAddress(input.value)
     });
@@ -26,40 +45,33 @@ fetch("whitelist.json")
           searchKeyFromAddress(input.value);
       }
     });
+    
     function searchKeyFromAddress (address) {
+      // key variables
       const leaf = keccak256(address)
       const proof = tree.getHexProof(leaf)
-      const temp = "You're key will be available soon!"
+      const temp = "Your key will be available soon! Check again later"
       const isValid = tree.verify(proof, leaf, rootHash)
-      function copyToNote(text) {
-        const tempInput = document.createElement("input");
-        tempInput.value = text;
-        document.body.appendChild(tempInput);
-        tempInput.select();
-        document.execCommand("copy");
-        document.body.removeChild(tempInput);
-      }
-      function truncateAddress (address) {
-        return `${address.substr(0, 5)}...${address.substr(
-            address.length - 5,
-            address.length
-        )}`;
-      }
-      mainWrapper.classList.add("start")
-      shadow.style.zIndex = "2";
-      const ANIMATION_TIME = 500
+      // style variables
       const wrapperWidth = (mainKeyWrapper.offsetWidth + "px").toString()
       const wrapperHeight = (mainKeyWrapper.offsetHeight + "px").toString()
-      mainKeyWrapper.style.minHeight = wrapperHeight
       let screenWidth = screen.width
       let isMobile = screenWidth < 768
+      const ANIMATION_TIME = 500
+      // starting style
+      mainWrapper.classList.add("start")
+      shadow.style.zIndex = "2";
+      // if wallet is in the whitelist
       if (isValid) {
+        // starting style for success
         mainWrapper.classList.remove("start")
         mainWrapper.classList.remove("error")
         mainWrapper.classList.remove("success")
+        mainKeyWrapper.style.minHeight = wrapperHeight
         mainKeyWrapper.style.minWidth = wrapperWidth
         shadow.classList.add("active")
         setTimeout(() => {
+          // changing style to key wrapper
           mainKeyWrapper.innerHTML = ""
           if (isMobile) {
             mainKeyWrapper.style.minWidth = "305px"
@@ -68,65 +80,79 @@ fetch("whitelist.json")
           }
           mainKeyWrapper.classList.remove("error")
           mainKeyWrapper.classList.add("success")
+
           setTimeout(() => {
             mainWrapper.classList.add("success")
+            successArea.innerHTML = "Congratulations, you're in WL!"
+            // area for the key
             const keyArea = document.createElement("span")
+            keyArea.style.color = "white"
+
+            // copy button setup
             const copyButton = document.createElement("button");
             copyButton.classList.add("copy-button");
             copyButton.innerHTML = "<img src='img/copy-icon.svg' alt='Copy' />";
-            if (isMobile) {
-              mainKeyWrapper.innerHTML = "<img src='img/key-icon.svg' alt='Copy' />:"
-              keyArea.append(truncateAddress(temp))
-              keyArea.style.color = "white"
+            const copyHover = document.createElement("div");
+            copyHover.classList.add("copy-hover");
+            copyHover.innerText = "Copy to clipboard";
+            copyButton.appendChild(copyHover)
+            copyButton.addEventListener("click", () => {
+              const textToCopy = temp;
+              copyToNote(textToCopy);
+              copyHover.innerText = "Copied!"
+              setTimeout(() => {
+                copyHover.innerText = "Copy to clipboard";
+              }, 1500);
+            });
+
+            // define area width and content if mobile
+            if (!isMobile) {
+              mainKeyWrapper.innerHTML = "<img src='img/key-icon.svg' alt='Copy' />"
+              keyArea.append(temp) // append result
             } else {
-              keyArea.append(temp)
-              const copyHover = document.createElement("div");
-              copyHover.classList.add("copy-hover");
-              copyHover.innerText = "Copy to clipboard";
-              copyButton.appendChild(copyHover)
-              copyButton.addEventListener("click", () => {
-                const textToCopy = temp;
-                copyToNote(textToCopy);
-                copyHover.innerText = "Copied!"
-      
-                setTimeout(() => {
-                  copyHover.innerText = "Copy to clipboard";
-                }, 1500);
-              });
-              mainKeyWrapper.innerHTML = "<img src='img/key-icon.svg' alt='Copy' /> Congratulations, you're in WL!"
+              mainKeyWrapper.innerHTML = "<img src='img/key-icon.svg' alt='Copy' />:"
+              keyArea.append(truncateAddress(temp)) // append result
             }    
             mainKeyWrapper.append(keyArea)
+            mainKeyWrapper.appendChild(copyButton);
+            // remove the shadow from key 
             shadow.classList.remove("active")
             setTimeout(() => {
               shadow.style.zIndex = "-2";
-            },400)
-            mainKeyWrapper.appendChild(copyButton);
+            },400)   
           }, ANIMATION_TIME);
         }, 400)
+      // if the wallet is not in the whitelist
       } else {
+        // starting style for error
+        successArea.innerHTML = ""
         mainWrapper.classList.remove("start")
         mainWrapper.classList.remove("success")
-        shadow.classList.add("active")
         mainKeyWrapper.style.minWidth = wrapperWidth
+        mainKeyWrapper.style.minHeight = wrapperHeight
+        shadow.classList.add("active")
         setTimeout(() => {
-          mainKeyWrapper.textContent = ""
-          if (isMobile) {
-            mainKeyWrapper.style.minWidth = "300px"
-          } else {
+          // define area width if mobile and change style
+          if (!isMobile) {
             mainKeyWrapper.style.minWidth = "425px"
+          } else {
+            mainKeyWrapper.style.minWidth = "300px"
           }
+          mainKeyWrapper.textContent = ""
           mainKeyWrapper.classList.remove("success")
           mainKeyWrapper.classList.add("error")
           setTimeout(() => {
             mainWrapper.classList.add("error")
+            mainKeyWrapper.textContent = "Sorry, your wallet is not in our whitelist"
+            // remove shadow for area
             shadow.classList.remove("active")
             setTimeout(() => {
               shadow.style.zIndex = "-2";
             },400)
-            mainKeyWrapper.textContent = "Sorry, your wallet is not in our whitelist"
           },ANIMATION_TIME)
         },400)
       }
+      // empty the input
       input.value = "";
     };
   })
